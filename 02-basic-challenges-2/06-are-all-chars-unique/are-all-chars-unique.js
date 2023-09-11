a8a8eaf20d251fe328b0a04976c8e37157613c66
Hugo <arceoh@gmail.com>
2023-09-11 14:29:30 -0700

    02-06

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,9 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const arr = str.split("");
+  const mySet = {};
+  arr.forEach((item) => (mySet[item] = 0));
+
+  return Object.keys(mySet).length === arr.length;
+}
 
 module.exports = areAllCharactersUnique;
