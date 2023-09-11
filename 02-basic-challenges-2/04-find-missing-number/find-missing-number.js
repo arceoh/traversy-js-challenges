5cd2ed63f9b2057f0a003a76cd1d392fff15c341
Hugo <arceoh@gmail.com>
2023-09-11 13:25:52 -0700

    02-04

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,5 +1,15 @@
 function findMissingNumber(arr) {
-  const sortedArr = arr.toSorted();
+  const sorted = arr.sort((a, b) => a - b);
+  let missing = -1;
+
+  for (let i = 0; i < sorted.length - 1; i++) {
+    if (missing === -1 && sorted[i] != sorted[i + 1] - 1) {
+      missing = sorted[i] + 1;
+      break;
+    }
+  }
+
+  return missing;
 }
 
 module.exports = findMissingNumber;
