8c69c48284c1f718134c869a42f6f61009206efa
Hugo <arceoh@gmail.com>
2023-09-11 15:56:59 -0700

    03-02

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,15 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  const sum = arr.reduce((total, current) => {
+    if (current % 2 === 0) {
+      console.log("current: ", current);
+      console.log("total: ", total);
+      console.log("POW: ", Math.pow(current, 2));
+      total = total + Math.pow(current, 2);
+    }
+    return total;
+  }, 0);
+
+  return sum;
+}
 
 module.exports = sumOfEvenSquares;
