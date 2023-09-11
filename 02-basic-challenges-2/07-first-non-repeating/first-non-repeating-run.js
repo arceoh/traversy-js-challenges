f01e962cfc0dce20911ab2eb01561027a52bb083
Hugo <arceoh@gmail.com>
2023-09-11 14:48:34 -0700

    02-07

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating-run.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating-run.js
@@ -1,7 +1,9 @@
-const findFirstNonRepeatingCharacter = require('./first-non-repeating');
+const findFirstNonRepeatingCharacter = require("./first-non-repeating");
 
-const result1 = findFirstNonRepeatingCharacter('programming');
-const result2 = findFirstNonRepeatingCharacter('abacddbec');
+const result1 = findFirstNonRepeatingCharacter("programming");
+const result2 = findFirstNonRepeatingCharacter("abacddbec");
+const result3 = findFirstNonRepeatingCharacter("aabccdeff"); // should return 'b'
 
 console.log(result1);
 console.log(result2);
+console.log(result3);
--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,13 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const pairs = [];
+
+  for (let i = 0; i < str.length; i = i + 2) {
+    if (str.at(i) != str.at(i + 1)) {
+      return str.at(i);
+    }
+  }
+
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
