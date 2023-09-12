642661b0dd3a7b6193a4e97d6299b5b2032fcfed
Hugo <arceoh@gmail.com>
2023-09-12 08:16:21 -0700

    03.04

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word-run.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word-run.js
@@ -1,5 +1,8 @@
-const highestScoringWord = require('./highest-scoring-word');
+const highestScoringWord = require("./highest-scoring-word");
 
-const result = highestScoringWord('Hello my name is xavier');
+const result = highestScoringWord("Hello my name is xavier");
+const result2 = highestScoringWord("what time are we climbing up the volcano");
+("volcano");
 
 console.log(result);
+console.log(result2);
--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,22 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const wordArr = str.split(" ");
+  let highScore = 0;
+  let highScoreWord = "";
+
+  wordArr.forEach((word) => {
+    let score = 0;
+
+    const charArr = word.split("");
+    charArr.forEach((char) => {
+      score = score + char.charCodeAt(0) - 96;
+    });
+
+    if (score >= highScore) {
+      highScore = score;
+      highScoreWord = word;
+    }
+  });
+  return highScoreWord;
+}
 
 module.exports = highestScoringWord;
