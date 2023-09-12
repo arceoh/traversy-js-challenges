5298c49c43a514fdb91cc0e201ddc8340dddc49c
Hugo <arceoh@gmail.com>
2023-09-12 09:15:01 -0700

    03.07

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-run.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-run.js
@@ -1,6 +1,8 @@
-const isValidIPv4 = require('./valid-ipv4.js');
+const isValidIPv4 = require("./valid-ipv4.js");
 
-const result1 = isValidIPv4('122.164.23.21');
-const result2 = isValidIPv4('122.164.23.21.33');
+const result1 = isValidIPv4("122.164.23.21");
+const result2 = isValidIPv4("122.164.23.21.33");
 
-console.log(result1, result2);
+console.log(result1);
+console.log("---");
+console.log(result2);
--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,18 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  // x . x . x . x
+  // x is called an octet
+  // octet must be a decimal value between 0 and 255
+  // must contain three periods and four octets
+
+  const arr = str.split(".");
+
+  if (arr.length != 4) return false;
+
+  const allValid = arr.every((octet) => {
+    return octet >= 0 && octet <= 255 && octet.at(0) != 0;
+  });
+
+  return allValid;
+};
 
 module.exports = isValidIPv4;
