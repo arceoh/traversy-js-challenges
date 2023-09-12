a1bc4b0a4fff6e04809068004a617f2be108ea27
Hugo <arceoh@gmail.com>
2023-09-12 08:23:13 -0700

    03.05

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,14 @@
-function validAnagrams() {}
+function validAnagrams(word1, word2) {
+  const arr1 = word1.split("").sort();
+  const arr2 = word2.split("").sort();
+
+  if (arr1.length != arr2.length) return false;
+
+  const str1 = arr1.join("");
+  const str2 = arr2.join("");
+  if (str1 != str2) return false;
+
+  return true;
+}
 
 module.exports = validAnagrams;
