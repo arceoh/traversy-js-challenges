b02e84be72680f37cf4c566f2f244ba4248ba54e
Hugo <arceoh@gmail.com>
2023-09-12 14:53:53 -0700

    04.05

--- 04-recursion/05-factorial/factorial.js
+++ 04-recursion/05-factorial/factorial.js
@@ -1,3 +1,10 @@
-function factorial() {}
+function factorial(num) {
+  // base case
+  if (num === 0) return 1;
+  if (num === 1) return 1;
+
+  // recursion
+  return num * factorial(num - 1);
+}
 
 module.exports = factorial;
