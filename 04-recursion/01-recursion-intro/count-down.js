13718ea123f1a647cc9876d3d2822b0d4f90fc4f
Hugo <arceoh@gmail.com>
2023-09-12 13:07:31 -0700

    04.01

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,14 @@
-function countDown() {}
+function countDown(num) {
+  // base case
+  if (num <= 0) {
+    console.log("All done!");
+    return "All done!";
+  }
+
+  // Recursion
+  console.log(num);
+  num--;
+  countDown(num);
+}
 
 module.exports = countDown;
