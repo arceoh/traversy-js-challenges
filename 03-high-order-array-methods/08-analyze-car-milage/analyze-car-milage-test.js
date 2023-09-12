e20a96841fd17d97dc2847d4738253d05f6600c1
Hugo <arceoh@gmail.com>
2023-09-12 10:00:12 -0700

    03.08

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage-test.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage-test.js
@@ -1,24 +1,24 @@
-const analyzeCarMileage = require('./analyze-car-milage');
+const analyzeCarMileage = require("./analyze-car-milage");
 
-test('Analyzing Car Mileage Data', () => {
+test("Analyzing Car Mileage Data", () => {
   const cars = [
-    { make: 'Toyota', model: 'Corolla', year: 2020, mileage: 25000 },
-    { make: 'Honda', model: 'Civic', year: 2019, mileage: 30000 },
-    { make: 'Ford', model: 'Mustang', year: 2021, mileage: 15000 },
+    { make: "Toyota", model: "Corolla", year: 2020, mileage: 25000 },
+    { make: "Honda", model: "Civic", year: 2019, mileage: 30000 },
+    { make: "Ford", model: "Mustang", year: 2021, mileage: 15000 },
   ];
 
   const analysis = analyzeCarMileage(cars);
 
   expect(analysis.averageMileage).toBeCloseTo(23333.33);
   expect(analysis.highestMileageCar).toEqual({
-    make: 'Honda',
-    model: 'Civic',
+    make: "Honda",
+    model: "Civic",
     year: 2019,
     mileage: 30000,
   });
   expect(analysis.lowestMileageCar).toEqual({
-    make: 'Ford',
-    model: 'Mustang',
+    make: "Ford",
+    model: "Mustang",
     year: 2021,
     mileage: 15000,
   });
--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,27 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  //   averageMileage: 23333.33,
+  //   highestMileageCar: { make: "Honda", model: "Civic", year: 2019, mileage: 30000 },
+  //   lowestMileageCar: { make: "Ford", model: "Mustang", year: 2021, mileage: 15000 },
+  //   totalMileage: 70000
+
+  const totalMileage = parseFloat(
+    cars
+      .reduce((accumulator, car) => {
+        return (accumulator = accumulator + car.mileage);
+      }, 0)
+      .toFixed(2)
+  );
+
+  const averageMileage = parseFloat((totalMileage / cars.length).toFixed(2));
+
+  const sortedByMilage = cars.sort((a, b) => {
+    return a.mileage - b.mileage;
+  });
+
+  const lowestMileageCar = sortedByMilage.at(0);
+  const highestMileageCar = sortedByMilage.at(-1);
+
+  return { totalMileage, averageMileage, lowestMileageCar, highestMileageCar };
+}
 
 module.exports = analyzeCarMileage;
