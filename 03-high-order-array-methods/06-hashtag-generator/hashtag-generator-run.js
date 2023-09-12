2ceb1198409706839d452d94bc0337c3c19b8ae9
Hugo <arceoh@gmail.com>
2023-09-12 08:38:02 -0700

    03.06

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator-run.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator-run.js
@@ -1,5 +1,9 @@
-const generateHashtag = require('./hashtag-generator');
+const generateHashtag = require("./hashtag-generator");
 
-const result = generateHashtag('hello world');
+const result = generateHashtag("hello world");
+const result2 = generateHashtag(" Hello there thanks for trying my Kata");
+const result3 = generateHashtag("    Hello     World   ");
 
 console.log(result);
+console.log(result2);
+console.log(result3);
--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,15 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  const cleanStr = str.trim().replace(/ +(?= )/g, "");
+  if (cleanStr.length === 0 || cleanStr.length > 140) return false;
+
+  console.log("CLEAN: ", cleanStr);
+  const arr = cleanStr.toLowerCase().split(" ");
+  console.log("ARR: ", arr);
+
+  const capArr = arr.map((word) => {
+    return `${word.at(0).toUpperCase()}${word.slice(1)}`;
+  });
+  return `#${capArr.join("")}`;
+}
 
 module.exports = generateHashtag;
