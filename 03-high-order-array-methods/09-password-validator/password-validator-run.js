407703f85302f60562d74db150702fba9a566b6b
Hugo <arceoh@gmail.com>
2023-09-12 10:21:58 -0700

    03.09

--- 03-high-order-array-methods/09-password-validator/password-validator-run.js
+++ 03-high-order-array-methods/09-password-validator/password-validator-run.js
@@ -1,6 +1,8 @@
-const validatePassword = require('./password-validator');
+const validatePassword = require("./password-validator");
 
-const result1 = validatePassword('Abc12345');
-const result2 = validatePassword('password');
+const result1 = validatePassword("Abc12345");
+const result2 = validatePassword("password");
 
-console.log(result1, result2);
+console.log(result1);
+console.log("----");
+console.log(result2);
--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,17 @@
-function validatePassword() {}
+function validatePassword(str) {
+  if (str.legth < 8) return false;
+
+  const arr = str.split("");
+  const hasDigits = arr.some((char) => Number.isInteger(parseInt(char)));
+  if (!hasDigits) return false;
+
+  const hasUpperCase = arr.some((char) => char == char.toUpperCase());
+  if (!hasUpperCase) return false;
+
+  const hasLowerCase = arr.some((char) => char == char.toLowerCase());
+  if (!hasLowerCase) return false;
+
+  return hasDigits && hasLowerCase && hasUpperCase;
+}
 
 module.exports = validatePassword;
