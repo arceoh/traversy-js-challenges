a86919471ed2d3dbf9e4a7149b44c5f806d9f13b
Hugo <arceoh@gmail.com>
2023-09-13 08:53:21 -0700

    04.09

--- 04-recursion/09-flatten-array/flatten-array-run.js
+++ 04-recursion/09-flatten-array/flatten-array-run.js
@@ -1,5 +1,5 @@
-const flattenArray = require('./flatten-array');
+const flattenArray = require("./flatten-array");
 
 const result = flattenArray([1, 2, 3, [4, 5, [6, 7, 8], 9], 10]);
 
-console.log(result);
+console.log("RESULT: ", result);
