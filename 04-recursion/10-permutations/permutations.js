0a69ef9db5518b7b16c4258be9ec0165fdbc5d40
Hugo <arceoh@gmail.com>
2023-09-13 09:23:08 -0700

    04.10

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,21 @@
-function permutations() {}
+function permutations(str) {
+  const result = [];
+  if (str === "") {
+    result.push("");
+    return result;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const firstChar = str.at(i);
+    const restOfString = str.slice(0, i) + str.slice(i + 1);
+    const subPermutations = permutations(restOfString);
+
+    for (let j = 0; j < subPermutations.length; j++) {
+      result.push(firstChar + subPermutations[j]);
+    }
+  }
+
+  return result;
+}
 
 module.exports = permutations;
