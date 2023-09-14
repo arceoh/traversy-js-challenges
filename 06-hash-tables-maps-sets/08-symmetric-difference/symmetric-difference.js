0119fc6f6946d3039ec49e9b81ee4d1b6e253338
Hugo <arceoh@gmail.com>
2023-09-14 15:11:11 -0700

    06.08

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,19 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  if (arr1.length === 0 && arr2.length === 0) return [];
+  const set = new Set([...arr1, ...arr2]);
+
+  const diffArr = [];
+
+  for (const item of set) {
+    if (
+      (arr1.indexOf(item) >= 0 && arr2.indexOf(item) === -1) ||
+      (arr1.indexOf(item) === -1 && arr2.indexOf(item) >= 0)
+    ) {
+      diffArr.push(item);
+    }
+  }
+
+  return diffArr;
+}
 
 module.exports = symmetricDifference;
