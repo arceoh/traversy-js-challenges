5da04de9778866ca0cfd915e5e1eccd62b4ee69c
Hugo <arceoh@gmail.com>
2023-09-14 09:43:26 -0700

    06.04

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,23 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  if (str === "") return new Map();
+
+  const arr = str
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9\s]/g, "")
+    .split(" ");
+
+  const wordCount = new Map();
+
+  arr.map((word) => {
+    if (!wordCount.has(word)) {
+      wordCount.set(word, 1);
+    } else {
+      wordCount.set(word, wordCount.get(word) + 1);
+    }
+  });
+
+  return wordCount;
+}
 
 module.exports = wordFrequencyCounter;
