437704cd4c4e7ed8775199e4598afdc1aee1dfc7
Hugo <arceoh@gmail.com>
2023-09-14 15:32:39 -0700

    06.09

--- 06-hash-tables-maps-sets/09-two-sum/two-sum-run.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum-run.js
@@ -1,5 +1,6 @@
-const twoSum = require('./two-sum');
+const twoSum = require("./two-sum");
 
 const result = twoSum([2, 7, 11, 15], 17);
+const result2 = twoSum([3, 3], 6);
 
 console.log(result);
--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,17 @@
-function twoSum() {}
+function twoSum(arr, sum) {
+  const result = [];
+
+  for (const [outerIndex, OuterNum] of arr.entries()) {
+    for (const [innerIndex, innerNum] of arr.entries()) {
+      if (outerIndex === innerIndex) continue;
+      if (OuterNum + innerNum === sum) {
+        result.push(outerIndex, innerIndex);
+        return result;
+      }
+    }
+  }
+
+  return result;
+}
 
 module.exports = twoSum;
