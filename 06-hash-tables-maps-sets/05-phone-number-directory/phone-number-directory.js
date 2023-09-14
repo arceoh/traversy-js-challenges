4652ad1d7d1a9eb0962b3eea49234f78c0aac092
Hugo <arceoh@gmail.com>
2023-09-14 10:15:46 -0700

    06.06

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,12 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+  const directory = new Map();
+
+  arr.map((item) => {
+    const [name, phone] = item.split(":");
+    directory.set(name, phone);
+  });
+
+  return directory;
+}
 
 module.exports = phoneNumberDirectory;
--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,20 @@
-function anagramGrouping() {}
+function anagramGrouping(arr) {
+  const groups = new Map();
+  arr.map((word) => {
+    const sortedWord = word.split("").sort().join("");
+    if (groups.has(sortedWord)) {
+      groups.set(sortedWord, [...groups.get(sortedWord), word]);
+    } else {
+      groups.set(sortedWord, [word]);
+    }
+  });
+
+  const result = [];
+  for (const [key, value] of groups) {
+    result.push(value);
+  }
+
+  return result;
+}
 
 module.exports = anagramGrouping;
