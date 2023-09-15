23b7c43f465c47efb2ee462cd9037d8b89ce4d95
Hugo <arceoh@gmail.com>
2023-09-15 09:33:23 -0700

    06.10

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive-run.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive-run.js
@@ -1,5 +1,47 @@
-const longestConsecutiveSequence = require('./longest-consecutive');
+const {
+  longestConsecutiveSequence,
+  longestConsecutiveSequenceArr,
+} = require("./longest-consecutive");
 
-const result = longestConsecutiveSequence([100, 4, 200, 1, 3, 2]);
+console.time("Warm Up");
+const result = longestConsecutiveSequence([1000000, 4, 200, 1, 3, 2]);
+const result2 = longestConsecutiveSequenceArr([0, 3, 7, 2, 5, 8, 4, 6, 9, 1]);
+console.timeEnd("Warm Up");
 
-console.log(result);
+console.time("Sets");
+const result3 = longestConsecutiveSequence(
+  Array.from({ length: 1000000 }, () => Math.floor(Math.random() * 1000000))
+);
+const result4 = longestConsecutiveSequence(
+  Array.from({ length: 1000000 }, () => Math.floor(Math.random() * 1000000))
+);
+const result5 = longestConsecutiveSequence(
+  Array.from({ length: 1000000 }, () => Math.floor(Math.random() * 1000000))
+);
+const result6 = longestConsecutiveSequence(
+  Array.from({ length: 1000000 }, () => Math.floor(Math.random() * 1000000))
+);
+console.log("Result 3: ", result3);
+console.log("Result 4: ", result4);
+console.log("Result 5: ", result5);
+console.log("Result 6: ", result6);
+console.timeEnd("Sets");
+
+console.time("Arrs");
+const result7 = longestConsecutiveSequenceArr(
+  Array.from({ length: 1000000 }, () => Math.floor(Math.random() * 1000000))
+);
+const result8 = longestConsecutiveSequenceArr(
+  Array.from({ length: 1000000 }, () => Math.floor(Math.random() * 1000000))
+);
+const result9 = longestConsecutiveSequenceArr(
+  Array.from({ length: 1000000 }, () => Math.floor(Math.random() * 1000000))
+);
+const result10 = longestConsecutiveSequenceArr(
+  Array.from({ length: 1000000 }, () => Math.floor(Math.random() * 1000000))
+);
+console.log("Result 7: ", result7);
+console.log("Result 8: ", result8);
+console.log("Result 9: ", result9);
+console.log("Result 10: ", result10);
+console.timeEnd("Arrs");
--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,44 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(arr) {
+  const mySet = new Set(arr);
+  let maxConsecutiveCount = 1;
 
-module.exports = longestConsecutiveSequence;
+  for (const num of mySet) {
+    if (mySet.has(num + 1)) {
+      let currentNum = num + 1;
+      let currentConsecutiveCount = 2; // two numbers in a row
+      while (mySet.has(currentNum + 1)) {
+        currentNum++;
+        currentConsecutiveCount++;
+      }
+      maxConsecutiveCount = Math.max(
+        maxConsecutiveCount,
+        currentConsecutiveCount
+      );
+    }
+  }
+  return maxConsecutiveCount;
+}
+function longestConsecutiveSequenceArr(arr) {
+  const sortedSet = new Set(arr.toSorted((a, b) => a - b));
+  const sortedArr = Array.from(sortedSet);
+  let maxConsecutiveCount = 1;
+
+  let currentConsecutiveCount = 1;
+  sortedArr.forEach((num, index) => {
+    if (index === 0) {
+      return;
+    }
+    if (num - sortedArr[index - 1] === 1) {
+      currentConsecutiveCount = currentConsecutiveCount + 1;
+      if (currentConsecutiveCount >= maxConsecutiveCount) {
+        maxConsecutiveCount = currentConsecutiveCount;
+      }
+    } else {
+      currentConsecutiveCount = 1;
+    }
+  });
+
+  return maxConsecutiveCount;
+}
+
+module.exports = { longestConsecutiveSequenceArr, longestConsecutiveSequence };
