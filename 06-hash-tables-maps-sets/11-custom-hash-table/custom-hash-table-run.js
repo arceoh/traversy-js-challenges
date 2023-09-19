e30212f98f2eb9b1eb0003006b86f00a07e30f1d
Hugo <arceoh@gmail.com>
2023-09-19 11:16:28 -0700

    06.11

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,10 @@
-const HashTable = require('./custom-hash-table');
+const HashTable = require("./custom-hash-table");
+
+const myHashTable = new HashTable();
+
+const result = myHashTable._hash("John", 55);
+console.log("Hash:", result);
+
+myHashTable.set("John");
+
+myHashTable.printTable();
--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
@@ -1,3 +1,27 @@
-class HashTable {}
+class HashTable {
+  constructor(limit = 14) {
+    this.storage = [];
+    this.limit = limit;
+  }
+
+  _hash(key, max) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % max;
+  }
+
+  printTable() {
+    console.log(this.storage);
+  }
+
+  set(key, value) {
+    const index = this._hash(key, this.limit);
+    if (this.storage[index] === undefined) {
+      this.storage[index] = [key, value];
+    }
+  }
+}
 
 module.exports = HashTable;
